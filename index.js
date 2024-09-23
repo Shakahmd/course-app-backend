5390057476acff2f384fc2c4ae7798e5eb75fd3e
shakahmd <shakkirahammad2002@gmail.com>
2024-09-23 18:12:50 +0530

    cors installed

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ import { configDotenv } from 'dotenv'
 import { connectDb } from './config/db.js'
 import { adminRouter } from './router/adminRouter.js'
 import { userRouter } from './router/userRouter.js'
+import cors from 'cors'
+
 
 configDotenv()
 const app = express()
 const port = process.env.PORT || 3002
+
 app.use(express.json())
+app.use(cors())
 connectDb()
 app.use(adminRouter)
 app.use(userRouter)
+
 
 app.listen(port,()=>{
     console.log(`server running on ${port}`)
